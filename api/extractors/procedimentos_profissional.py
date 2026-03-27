104f3bc89e776ee7e9fa504905b6562993d9a4f5
ranielcaria <ashjogar@gmail.com>
2026-03-27 15:36:40 -0300

    added: code logic for 'Transferência de Produtos' report.

---
+++ api/extractors/procedimentos_profissional.py
@@ -0,0 +1,53 @@
+from api.core.base import BaseExtractor
+from api.core.transformers import parse_datetime
+
+class ProcedimentosPorProfissional(BaseExtractor):
+    file_path = 'api/input/Procedimentos realizados por profissional - Analítico.xls'
+    
+    table = 'procedimentos_por_profissional'
+    columns = ['unidade','profissional', 'especialidade', 'procedimento', 'quantidade', 'data_hora', 'sexo', 'idade']
+    conflict = '(unidade, profissional, especialidade, procedimento, data_hora)'
+    
+    def transform(self, df):
+        registros = []
+        unidade = profissional = procedimento = data = quantidade = None
+        
+        for _, row in df.iterrows():
+            if 'Unidade:' in str(row.iloc[1]):
+                unidade = str(row.iloc[1]).replace('Unidade: ', '').title()
+                
+            if 'Profissional:' in str(row.iloc[4]):
+                profissional = str(row.iloc[4]).replace('Profissional: ', '').title()
+                
+            if 'Procedimento:' in str(row.iloc[5]):
+                procedimento = str(row.iloc[5]).title().split(' - ')[-1]
+                
+            if '/' in str(row.iloc[6]):
+                data = parse_datetime(str(row.iloc[6]))
+    
+            if not data:
+                continue
+            
+            registros.append({
+                'unidade': unidade,
+                'profissional': profissional,
+                'procedimento': procedimento,
+                'quantidade': str(row.iloc[45]) if 'nan' not in str(row.iloc[45]) else quantidade,
+                'data_hora': data,
+                'sexo': row.iloc[36],
+                'idade': row.iloc[38]
+            })
+            
+        return registros
+        
+if __name__ == "__main__":
+    extractor = ProcedimentosPorProfissional()
+    
+    dados_finais = extractor.run()
+    
+    print('--- header ---')
+    
+    for r in dados_finais[:5]:
+        print(r)
+    
+    print(f"--- fim, {len(dados_finais)} registros ---")
