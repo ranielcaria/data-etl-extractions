447c9fcc9c9b36f9381d693ab4e6cf92f67af479
ranielcaria <ashjogar@gmail.com>
2026-03-31 15:52:55 -0300

    added: input report file for logic button in main GUI

---
+++ api/core/input.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from tkinter import filedialog, messagebox
+
+def input_file():
+    home_dir = os.path.expanduser('~')
+    downloads_dir = os.path.join(home_dir, 'Downloads')
+
+    source_file = filedialog.askopenfilename(
+        initialdir=downloads_dir,
+        title='Selecione um Relatório',
+        filetypes=[('Excel File', '*.xls *.xlsx')]
+    )
+
+    if not source_file or source_file == '':
+        return
+    
+    destination_folder = './input'
+    filename = os.path.basename(source_file)
+    destination_folder = os.path.join(destination_folder, filename)
+
+    try:
+        shutil.move(source_file, destination_folder)
+        messagebox.showinfo('Sucesso!')
+    except shutil.Error as e:
+        messagebox.showerror(f'Falha! Erro: {e}')
+    except OSError as e:
+        messagebox.showerror(f'Falha! Erro: {e}')
