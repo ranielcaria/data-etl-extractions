d347f4212d980b1c7fbd23713c7ed92d53ad9a6a
ranielcaria <ashjogar@gmail.com>
2026-04-08 15:12:15 -0300

    pipeline for orchestration

--- api/core/pipeline.py
+++ api/core/pipeline.py
@@ -1,6 +1,7 @@
-from api.core.conn import postgres_connection
 from api.core.insert import PostgresLoader
-
+from tkinter import messagebox as msg
+from shutil import move as move
+import os
 
 class ETLPipeline:
     def __init__(self, extractor, connection):
@@ -8,12 +9,25 @@ class ETLPipeline:
         self.connection = connection
         self.loader = PostgresLoader()
 
+    def move_file(self, extractor):
+        from datetime import datetime
+        try:
+            move(extractor.file_path, \
+                 extractor.file_path.replace("input", "archive")
+                 .replace(".xls", \
+                f" [{datetime.now().strftime('%d-%m-%Y %H-%M')}].xls"))
+        except FileNotFoundError:
+            msg.showerror('File Not Found')
+        except Exception:
+            msg.showerror(f'Error: {Exception}')
+
     def run(self):
-        rows = self.extractor.run()
+        extractor = self.extractor()
+        rows = extractor.run()
         self.loader.insert(
             conn=self.connection,
-            table=self.extractor.table,
-            columns=self.extractor.columns,
+            table=extractor.table,
+            columns=extractor.columns,
             rows=rows,
-            conflict=self.extractor.conflict,
         )
+        self.move_file(extractor)
