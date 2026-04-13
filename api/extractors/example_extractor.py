e542edc1d85fbf8099ef99fccf6c7fe993cffb94
ranielcaria <ashjogar@gmail.com>
2026-04-13 15:27:34 -0300

    comments for code explanation

--- api/extractors/example_extractor.py
+++ api/extractors/example_extractor.py
@@ -1,11 +1,23 @@
 from api.core.base import BaseExtractor
 
 class example_extractor(BaseExtractor):
+
+    # Toda classe nova DEVE ter essas três variáveis
+    # sendo 'file_path' o caminho do arquivo excel que você vai extrair os dados,
+    # 'table' o nome da tabela que você pretende inserir no banco de dados.
+    # e por último, 'columns' que são as colunas da sua tabela
+    # OBS: os nomes são case-sensitive, então escreva o nome de sua tabela, path e coluna
+    # exatamente da forma como estão. 
+
     file_path = 'api/input/example.xls'
     table = 'example'
     columns = ['example']
 
     def transform(self, df):
+        # Aqui vai ir a lógica da sua extração. 
+        # Não se esqueça que essa função PRECISA retornar algo. de preferẽncia,
+        # uma lista
+
         registros = []
 
         for _, row in df.iterrows():
@@ -18,6 +30,9 @@ class example_extractor(BaseExtractor):
         return registros
     
 def run_example(conn):
+    # Aqui temos um import do pipeline. só ponha o nome da sua classe
+    # dentro da função chamada, e chame o arquivo 'conn' junto.
+
     from api.core.pipeline import ETLPipeline
     pipe = ETLPipeline(example_extractor, conn)
     pipe.run()
